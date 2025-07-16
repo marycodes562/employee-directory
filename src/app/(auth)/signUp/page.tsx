28c603e763b3eb0a2a3d5efe21513956bd152497
Rufaro <farohanna@gmail.com>
2025-07-16 21:05:54 +0100

    Feat: Added background images and useState to the login and sign up pages

--- src/app/(auth)/signUp/page.tsx
+++ src/app/(auth)/signUp/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from 'react';
+import React, { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
@@ -10,16 +10,33 @@ import styles from './page.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function SignUp() {
+	const [firstName, setFirstName] = useState('');
+	const [lastName, setLastName] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	const [signedUp, setSignedUp] = useState(false);
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		if (firstName && lastName) {
+			alert('You are logged in')
+		}
+	}
+
 	return (
 		<div className={styles.page}>
 			<Card border="primary" className={styles.form} style={{ width: '20rem' }}>
-						<Form>								
+				
+						<Form onSubmit={handleSubmit}>
+																	
 									<FloatingLabel
 									controlId="floatingInput"
 									label="First Name"
 									className="mb-3"
 									>
-									<Form.Control type="text" placeholder="First Name" />
+									<Form.Control type="text" placeholder="First Name" onChange={(e) => setFirstName(e.target.value)}/>
 									</FloatingLabel>
 									
 									<FloatingLabel
@@ -27,7 +44,7 @@ export default function SignUp() {
 									label="Last Name"
 									className="mb-3"
 									>
-									<Form.Control type="text" placeholder="Last Name" />
+									<Form.Control type="text" placeholder="Last Name" onChange={(e) => setLastName(e.target.value)} />
 									</FloatingLabel>
 									
 									<FloatingLabel
@@ -35,7 +52,7 @@ export default function SignUp() {
 									label="Email address"
 									className="mb-3"
 									>
-									<Form.Control type="email" placeholder="name@example.com" />
+									<Form.Control type="email" placeholder="name@example.com" onChange={(e) => e.target.value}/>
 									</FloatingLabel>
 									
 									<FloatingLabel
@@ -46,7 +63,7 @@ export default function SignUp() {
 									<Form.Control type="password" placeholder="Password" />
 									</FloatingLabel>
 			
-									<Button type="submit" className='signUpButton'>Sign Up</Button>
+									<Button type="submit" className={styles.signUpButton}>Sign Up</Button>
 						</Form>
 					</Card>
 		</div>
