59b8de3fe31c97ec27586d96f63437256c1e7f99
Mary.Codes <marysylvester562@gmail.com>
2025-08-16 09:11:46 +0100

    fix: changed metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,14 @@
 "use client";
-
-import Image from "next/image";
 import styles from "./page.module.css";
-import SignUp from "./(auth)/signUp/page";
-import LoginPage from "./(auth)/login/page";
-import ProfilePage from "@/components/profile/profile";
+
 import EmployeeInfo from "./employeeInfo/page";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Home() {
 	return (
 		<div className={styles.page}>
-			<main className={styles.main}>
 			<EmployeeInfo />
-				
-			</main>
-			<footer className={styles.footer}>
-				
-			</footer>
 		</div>
 	);
 }
