120d536f0578f6ce601bf1c6c1d5bba986933817
Rufaro <farohanna@gmail.com>
2025-10-25 13:57:04 +0100

    Feat: Added a loading component for the personnel table that contains a loading spinner

--- src/app/employeeInfo/page.tsx
+++ src/app/employeeInfo/page.tsx
@@ -17,6 +17,7 @@ import { Trash2 } from "@deemlol/next-icons";
 import { Plus } from "@deemlol/next-icons";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { isAdmin, useUserRole } from '@/hooks/useUserRole';
+import Loading from '@/components/profile/loading';
 
 
 export default function EmployeeInfo() {
@@ -43,10 +44,13 @@ export default function EmployeeInfo() {
     const [search, setSearch] = useState('');
     const [searchResults, setSearchResults] = useState([]);
 
+    const [isloading, setIsLoading] = useState(true);
+
     /* ------------- Get Employee info using a firebase query -------------- */
     async function loadEmployees() {
          const data = await getEmployees();
          setPersonnel(data);
+         setIsLoading(false);
     }
 
     useEffect(() => {
@@ -188,7 +192,14 @@ export default function EmployeeInfo() {
 
     } else {
         /*------------------------- All Employee Data ----------------------------------*/
+        if (isloading) {
+            
+            return <Loading />
+
+        } else {
+
         return (
+            
             < div className={styles.table} >
                 <Table responsive>
                     <thead>
@@ -228,6 +239,7 @@ export default function EmployeeInfo() {
                 </div>
             )
         } 
+    }
 
     }
 
